5f13bd75edfadc45f1c383dbedf13dab6cca2cbf
zhangyanhua <939593210@qq.com>
2022-07-14 08:49:22 +0800

    feat:scheduler 的實現

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -2,7 +2,7 @@
 let activeEffect
 class ReactiveEffect{
     public _fn:any
-    constructor(fn:Function) {
+    constructor(fn:Function,public scheduler?) {
         this._fn=fn
     }
     run(){
@@ -10,8 +10,8 @@ class ReactiveEffect{
        return this._fn()
     }
 }
-export function effect(fn){
-   const _effect = new ReactiveEffect(fn);
+export function effect(fn,options:any={}){
+   const _effect = new ReactiveEffect(fn, options.scheduler);
    _effect.run();
    return _effect.run.bind(_effect);
 }
@@ -22,10 +22,13 @@ export function track(target,key){
         depsMap = new Map();
         targetMap.set(target,depsMap);
     }
+    console.log('depsMap===',depsMap)
     let dep = depsMap.get(key);
+    console.log("dep===", dep);
+    
     if(!dep){
         dep = new Set();
-        depsMap.set(target,key)
+        depsMap.set(key,dep)
     }
     dep.add(activeEffect);
     // const dep =new Set()
@@ -34,7 +37,13 @@ export function trigger(target,key){
     let depsMap = targetMap.get(target);
      let dep = depsMap.get(key);
      for (const effect of dep){
-         effect.run()
+         console.log('effect',effect)
+         if(effect.scheduler){
+             effect.scheduler()
+         }else{
+         effect.run();
+             
+         }
      }
 
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -27,3 +27,29 @@ describe("effect", () => {
   })
 
 });
+  it("scheduler", () => {
+    let dummy;
+    let run: any;
+    // 通過effect 第二個參數
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    // should be called on first trigger
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    // should not run yet
+    expect(dummy).toBe(1);
+    // manually run
+    run();
+    // should have run
+    expect(dummy).toBe(2);
+  });
